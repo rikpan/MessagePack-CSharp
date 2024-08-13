0f925f8a8ee3e914e59135f10a8050f61c7c1afc
zhangyg <rikpan@163.com>
2024-08-14 07:43:22 +0800

    修改Unity打包配置

--- src/MessagePack.UnityClient/build.bat
+++ src/MessagePack.UnityClient/build.bat
@@ -13,7 +13,7 @@ IF NOT EXIST "%LogDirectory%" MD "%LogDirectory%"
 ECHO Log files will be created under "%LogDirectory%"
 
 echo on
-start "Unity PACKAGE" /wait "c:\Program Files\unity\editor\Unity.exe" -batchmode -quit -nographics -buildTarget standalone -projectPath "%~dp0\" -executeMethod PackageExport.Export -logfile "%LogDirectory%\unity_package.log"
+start "Unity PACKAGE" /wait "D:\Tools\Unity\6000.0.11f1\editor\Unity.exe" -batchmode -quit -nographics -buildTarget standalone -projectPath "%~dp0\" -executeMethod PackageExporter.Export -logfile "%LogDirectory%\unity_package.log"
 @IF ERRORLEVEL 1 (
     ECHO Package build FAILED
     exit /b %ERRORLEVEL%
